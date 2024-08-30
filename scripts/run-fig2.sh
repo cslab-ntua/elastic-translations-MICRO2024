04bf63972f69001cb9881dd2651652fd726a5210
Stratos Psomadakis <774566+psomas@users.noreply.github.com>
2024-08-30 12:46:21 +0300

    Minor fixes for run-fig2.sh

--- scripts/run-fig2.sh
+++ scripts/run-fig2.sh
@@ -4,6 +4,25 @@
 
 set -o pipefail -o errexit
 
+ok() {
+        echo -e "[\033[0;32mOK\033[0m] ${@}"
+}
+
+fail() {
+        echo -e "[\033[0;31mFAILED\033[0m] ${@}"
+        exit 1
+}
+
+cleanup() {
+	FAILED=0
+	[ $? -ne 0 ] && FAILED=1
+	while popd &>/dev/null; do true; done
+	clear_htlb.sh
+	[ ${FAILED} -eq 1 ] && fail "${0} failed, exiting..."
+	ok "${0} finished, exiting..."
+}
+trap cleanup EXIT
+
 # init env
 # This script will get called again inside the VM, to actually call run.sh
 if [ "${TYPE}" == "vm" ]; then
@@ -13,7 +32,6 @@ else
 fi
 source "${BASE}/env/base.env"
 pushd "${BASE}"
-
 # For frag2 we target a non-fragmented machine -- unset frag target
 unset FRAG_TARGET
 # We only use MODE for ET / Hawkeye / Trident
@@ -37,6 +55,8 @@ fi
 
 sizes="pte hptec thp hpmd hpmdc hpud"
 for sz in ${sizes}; do
+	clear_htlb.sh
+
 	# PGSZ defines the requested translation (page) size to use, see bin/run.sh for
 	# more info
 	export PGSZ="${sz}"
@@ -47,5 +67,5 @@ for sz in ${sizes}; do
 
 	# We use a separate script for the virtualized results
 	$(dirname ${0})/run-fig2-vm.sh
+	clear_htlb.sh
 done
-
